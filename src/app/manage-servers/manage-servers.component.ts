b6bd310c65e36e371083139043a52c32e08323c9
salimboughanmi <boughanmi1salim@gmail.com>
2024-09-18 01:52:57 +0100

    manage server

--- src/app/manage-servers/manage-servers.component.ts
+++ src/app/manage-servers/manage-servers.component.ts
@@ -7,4 +7,50 @@ import { Component } from '@angular/core';
 })
 export class ManageServersComponent {
 
+  listServers = [
+    {
+      nom: 'Production Server',
+      type: 'small',
+      statut: 'stable',
+      date_d: new Date(2020, 3, 14),
+    },
+    {
+      nom: 'Development Server',
+      type: 'large',
+      statut: 'critical',
+      date_d: new Date(2020, 3, 14),
+    },
+    {
+      nom: 'Test Development Server',
+      type: 'small',
+      statut: 'offline',
+      date_d: new Date(2020, 3, 14),
+    },
+    {
+      nom: 'Nidhal Server',
+      type: 'large',
+      statut: 'stable',
+      date_d: new Date(2020, 3, 14),
+    },
+  ];
+
+  selectedStatut = '';
+
+  addServer() {
+    this.listServers.push({
+      nom: 'NEW SERVER',
+      type: 'large',
+      statut: 'stable',
+      date_d: new Date(2020, 3, 14),
+    });
+  }
+
+  affecterClass(st) {
+    return {
+      'list-group-item-success': st == 'stable' ? true : false,
+      'list-group-item-danger': st == 'critical' ? true : false,
+      'list-group-item-warning': st == 'offline' ? true : false,
+    };
+  }
+
 }
